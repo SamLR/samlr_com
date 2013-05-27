bfa8b4f0a793db843a9dc5d9537a1d43dc268f7b
Sam Cook <sam@samlr.com>
2013-05-27 23:59:14 +0100

    working version of publish script

--- publish.sh
+++ publish.sh
@@ -6,11 +6,12 @@
 set -o errexit
 set -u nounset
 
-tachikoma_dir="externals/Tachikoma"
+tachikoma="externals/Tachikoma/tachikoma.py"
 site_dir="samlr_com"
 publish_dir="../published"
-exe="python3 $tachikoma_dir/tachikoma.py samlr_com"
-# exe="python3 test.py"
+exe="python3 $tachikoma $site_dir"
+publish_repo="publish"
+publish_branch="master"
 uncommited_changes=`git status --porcelain --ignore-submodules`
 if [[ $uncommited_changes ]]; then
 	echo "There are uncommited changes, please commit them or revert them before publishing"
@@ -18,10 +19,14 @@ if [[ $uncommited_changes ]]; then
 	exit 1
 fi
 
+commit_msg=`git log -1 --pretty=%B`
+
 # Re-run Tachikoma and generate the site
 $exe 
-cp -r $site_dir $publish_dir
+cp -r $site_dir/_site/* $publish_dir
 echo "Now pushing to web"
 cd $publish_dir
-ls -l
-# git push web
+git add -A 
+git commit -m "$commit_msg"
+git push $publish_repo $publish_branch 
+exit 
